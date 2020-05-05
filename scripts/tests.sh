c76043469adbad0b31702dd541592b803bc50d7a
Azize Elamrani <azize.elamrani@gmail.com>
2020-05-05 13:40:43 +0200

    Create views before to run tests

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
+newman run postman/showcase/Gravitee.io-Showcase-Create.View.json -e postman/env/Gravitee.io-Localhost-Environment.json -d postman/showcase/views.json --bail;
 for f in postman/test/*;do if [[ -f $f ]]; then newman run $f -e postman/env/Gravitee.io-Localhost-Environment.json --bail; fi; done;
